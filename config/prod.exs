290e1e207b98c1ea82210e8fff84158e491a7587
MainShayne233 <shaynetremblay@hotmail.com>
2016-11-03 02:20:43 -0400

    fixed

--- config/prod.exs
+++ config/prod.exs
@@ -15,6 +15,7 @@ config :euler, Euler.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -56,10 +57,9 @@ config :logger, level: :info
 #     config :euler, Euler.Endpoint, server: true
 #
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-# Configure your database
-  config :my_app, MyApp.Repo,
+# Configure secret_key_base for cookie-based session storage
+
+  config :my_app, Euler.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   size: 20 # The amount of database connections in the pool

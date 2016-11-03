1da83cc23f1fbc2eeb7cf290682adacc8a752015
MainShayne233 <shaynetremblay@hotmail.com>
2016-11-03 02:19:45 -0400

    removed prod.secret

--- config/prod.exs
+++ config/prod.exs
@@ -58,4 +58,8 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# Configure your database
+  config :my_app, MyApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  size: 20 # The amount of database connections in the pool

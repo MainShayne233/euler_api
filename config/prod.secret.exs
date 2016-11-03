dffc9f74267b991b42ebeefa3d02c9752ff088ed
MainShayne233 <shaynetremblay@hotmail.com>
2016-11-03 01:49:22 -0400

    added prod.secret

---
+++ config/prod.secret.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :euler, Euler.Endpoint,
+  secret_key_base: "qx0C3jphAsztUb+uaadpSt1dX1tE+niWYbUX3HODGG5i+65iWtbTrtVDugx7QVot"
+
+# Configure your database
+config :euler, Euler.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("DATABASE_USERNAME"),
+  password: System.get_env("DATABASE_PASSWORD"),
+  database: "euler_prod",
+  pool_size: 20
